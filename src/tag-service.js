fa0f3f3bba5837cb2438b4df09c2f23c51dc30f9
dohyeon5626 <kodohyeon71@gmail.com>
2022-10-16 23:14:31 +0900

    Change number expression

--- src/tag-service.js
+++ src/tag-service.js
@@ -3,8 +3,6 @@ let appendFeedLiTagElement = (feedButton) => {
     li.setAttribute("role", "presentation");
     li.setAttribute("data-view-component", "true");
     li.classList.add("d-inline-flex");
-    li.setAttribute("aria-selected", "false");
-    li.setAttribute("tabindex", "-1");
     li.appendChild(feedButton);
     document.querySelector("ul.UnderlineNav-body.list-style-none").appendChild(li);
 }
@@ -18,6 +16,8 @@ let getFeedButtonTagElement = (buttonText) => {
     button.setAttribute("aria-controls", "panel-3");
     button.setAttribute("data-view-component", "true");
     button.classList.add("UnderlineNav-item");
+    button.setAttribute("aria-selected", "false");
+    button.setAttribute("tabindex", "-1");
     button.onclick = () => {
         document.querySelector("div.UnderlineNav-actions.js-feedback-link.d-flex").hidden = true;
     }
@@ -90,7 +90,7 @@ let getRepoTag = (user, repo, repoDescription, repoLanguageColor, repoLanguage, 
                                 <svg class="octicon octicon-star mr-1" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path>
                                 </svg>
-                                ${repoStarAmount}
+                                ${abbreviateNumber(repoStarAmount)}
                             </a>
                         </span>
                     </p>
@@ -142,4 +142,21 @@ let applyClearButtonEvent = (panel, watcher) => {
             });
         });
     };
+}
+
+let abbreviateNumber = (value) => {
+    var newValue = value;
+    if (value >= 1000) {
+        var suffixes = ["", "k", "m", "b","t"];
+        var suffixNum = Math.floor( (""+value).length/3 );
+        var shortValue = '';
+        for (var precision = 2; precision >= 1; precision--) {
+            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            if (dotLessShortValue.length <= 2) { break; }
+        }
+        if (shortValue % 1 != 0)  shortValue = shortValue.toFixed(1);
+        newValue = shortValue+suffixes[suffixNum];
+    }
+    return newValue;
 }
