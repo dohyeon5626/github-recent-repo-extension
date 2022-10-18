1c0d9613ccd2b7ddd8720a5efd40f547a1e226a1
dohyeon5626 <kodohyeon71@gmail.com>
2022-10-18 23:12:01 +0900

    Fix undefined error

--- src/org.js
+++ src/org.js
@@ -3,21 +3,23 @@ for (let i=0; i<aTagList.length; i++) {
     aTagList[i].onclick = () => {
         let metaTag = document.querySelector("meta[name=octolytics-actor-login]");
         let info = aTagList[i].href.split('/');
-        let user = info[3];
-        let repo = info[4].split((/[!,@,#,$,%,^,&,*,(,),+,?,>,<,~,₩]/g))[0];
+        if (info.length >= 5) {
+            let user = info[3];
+            let repo = info[4].split((/[!,@,#,$,%,^,&,*,(,),+,?,>,<,~,₩]/g))[0];
 
-        if (metaTag != undefined && aTagList[i].href.indexOf("https://github.com/") != -1 && user != undefined && repo != undefined) {
-            let watcher = metaTag.content;
+            if (metaTag != undefined && aTagList[i].href.indexOf("https://github.com/") != -1 && user != undefined && repo != undefined) {
+                let watcher = metaTag.content;
 
-            getUserRepoList(watcher, (infoMap, userRepoInfoList) => {
-                userRepoInfoList = userRepoInfoList.filter(repoInfo => repoInfo.repo != repo || repoInfo.user != user);
-                userRepoInfoList.unshift({
-                    user: user,
-                    repo: repo
+                getUserRepoList(watcher, (infoMap, userRepoInfoList) => {
+                    userRepoInfoList = userRepoInfoList.filter(repoInfo => repoInfo.repo != repo || repoInfo.user != user);
+                    userRepoInfoList.unshift({
+                        user: user,
+                        repo: repo
+                    });
+                    infoMap.set(watcher, userRepoInfoList.splice(0, 30));
+                    setUserRepoList(infoMap);
                 });
-                infoMap.set(watcher, userRepoInfoList.splice(0, 30));
-                setUserRepoList(infoMap);
-            });
+            }
         }
     }
 }
