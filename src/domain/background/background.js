d1b77ef29c9460acb8ed0d4b1e26cf2757676c14
dohyeon5626 <kodohyeon71@gmail.com>
2022-10-27 00:54:56 +0900

    Fix not save repo history

--- src/domain/background/background.js
+++ src/domain/background/background.js
@@ -16,3 +16,37 @@ chrome.runtime.onMessage.addListener(
         return true;
     }
 );
+
+let registRepo = () => {
+    let metaTag = document.querySelector("meta[name=octolytics-actor-login]");
+    let info = location.href.split('/');
+    if (info.length >= 5) {
+        let user = info[3];
+        let repo = info[4].split((/[!,@,#,$,%,^,&,*,(,),+,?,>,<,~,₩]/g))[0];
+
+        if (metaTag != undefined != -1 && user != undefined && repo != undefined) {
+            let watcher = metaTag.content;
+
+            getUserRepoList(watcher, (infoMap, userRepoInfoList) => {
+                userRepoInfoList = userRepoInfoList.filter(repoInfo => repoInfo.repo != repo || repoInfo.user != user);
+                userRepoInfoList.unshift({
+                    user: user,
+                    repo: repo
+                });
+                infoMap.set(watcher, userRepoInfoList.splice(0, 30));
+                setUserRepoList(infoMap);
+            });
+        }
+    }
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.url) {
+        if(changeInfo.url.includes("https://github.com")) {
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                function: registRepo
+            });
+        }
+    }
+});
